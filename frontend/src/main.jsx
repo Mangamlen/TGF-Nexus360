7064c3659858f866715781ad0793a0a6f6d84bc6
lenin laishram <leninlaishram8@gmail.com>
2025-12-04 17:40:57 +0530

    new

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,23 +1,10 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./pages/Login";
-import Dashboard from "./pages/Dashboard";
-import EmployeeForm from "./pages/EmployeeForm";
-import AddBeneficiary from "./pages/AddBeneficiary";
-import "./index.css";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
-function App(){
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Login/>} />
-        <Route path='/dashboard' element={<Dashboard/>} />
-        <Route path='/employee/add' element={<EmployeeForm/>} />
-        <Route path='/beneficiary/add' element={<AddBeneficiary/>} />
-      </Routes>
-    </BrowserRouter>
-  );
-}
-
-createRoot(document.getElementById("root")).render(<App/>);
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
--- frontend/src/services/api.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-const api = axios.create({ baseURL: "http://localhost:5000/api" });
-api.interceptors.request.use(cfg => {
-  const token = localStorage.getItem("token");
-  if (token) cfg.headers.Authorization = "Bearer " + token;
-  return cfg;
-});
-export default api;
